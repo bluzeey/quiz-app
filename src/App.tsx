7146a71a369c9badf6a0de754b48e2c178ae5763
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-30 09:06:50 +0530

    Adding some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-
+import QuestionCard from "./components/QuestionCard";
 function App() {
+  
+  const startTrivia=async()=>{
+
+  }
+
+  const checkAnswer=(e:React.MouseEvent<HTMLButtonElement>)=>{
+
+  }
+
+  const nextQuestion=()=>{
+
+  }
+
   return (
-    <div>Quiz</div>
+    <div>
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">
+        Score:
+      </p>
+      <p>Loading Questions...</p>
+      <QuestionCard/>
+      <button className="next" onClick={nextQuestion}>
+        
+      </button>
+    </div>
   );
 }
 
